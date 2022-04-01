3af47a802d8e131c48f09e8c343ef2bec6ef989c
cdmccune <curtmccune@gmail.com>
2022-04-01 10:21:40 -0700

    finished orders.js

--- orders.js
+++ orders.js
@@ -29,7 +29,17 @@
     stored on the object.
 */
 
-//CODE HERE
+class Ticket {
+    constructor (items, orderTime, customerID) {
+        this.items = items
+        this.orderTime = orderTime
+        this.customerID = customerID
+        this.status = `queued`
+    } updateStatus = (newStatus) => {
+        this.status = newStatus
+        console.log(`The order for the customer ${this.customerID} is now ${this.status}`)
+    }
+}
 
 
 
@@ -44,8 +54,8 @@
     customer: 575
 */
 
-//CODE HERE
-
+const firstTicket = new Ticket([`pizza`,`bread`,`soda`], `7:03 PM`, 575)
+// console.log(firstTicket)
 
 /*
     Call the `updateStatus` method on
@@ -53,4 +63,4 @@
     'cooking'
 */
 
-//CODE HERE
+firstTicket.updateStatus(`cooking`)
